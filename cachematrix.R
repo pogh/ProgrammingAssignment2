547fd823bd5c8c67c0d67062b5d438a9e38faf1d
pogh <github@spellman.info>
2015-02-15 18:24:55 +0100

    Programming Assignment 2: Lexical Scoping
    Johns Hopkins University Data Science
    Course 2: Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Johns Hopkins University Data Science
+## Course 2: Programming
+## Programming Assignment 2: Lexical Scoping
 
-## Write a short comment describing this function
+## There’s way too much commenting, but hey, it’s an assignment
 
+## This initialises a 'factor' function,
+## i.e. a function which lists other functions.
+## We can use this store matrices in a cache.
 makeCacheMatrix <- function(x = matrix()) {
-
+  iM <- NULL                           # initialise variable inverse matrix cache 
+  
+  set <- function(y) {                 # set function takes matrix y
+    x <<- y                            # and assigns it to x (our cached matrix)
+    iM <<- NULL                        # and resets the inverse matrix cache
+  }
+  get <- function() { 
+    x                                  # get function just returns cached matrix
+  }
+  
+  setInverse <- function(invMatrix) {  # set function takes inverse matrix invMatrix
+    iM <<- invMatrix                   # and assigns it our inverse matrix cache variable
+                                       # we have to use <<- because iM not available to set 
+                                       # in environment of this function
+  }
+  getInverse <- function() {           
+    iM                                 # just return inverse matrix cache
+  }
+  
+  list(                                # return our functions as a list
+    set = set, 
+    get = get,
+    setInverse = setInverse,
+    getInverse = getInverse
+  )
 }
 
-
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
+## The result is 'solved' the first time and cached.
+## Subsequent calls return the cached result.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  invMatrix <- x$getInverse()               # Get the cached inverse matrix (might be NULL)
+  
+  if(!is.null(invMatrix)) {                 # If the cached inverse matrix was not empty
+    print("Returning inverse matrix from cache.")
+    return(invMatrix)                       # return cached inverse matrix and we're done
+  }
+                                            # Otherwise...
+  print("Solving inverse matrix")
+  invMatrix <- solve(x$get())               # 'Solve' the matrix
+  x$setInverse(invMatrix)                   # and assign it to the cache for next time
+  
+  invMatrix                                 # and return it
 }
